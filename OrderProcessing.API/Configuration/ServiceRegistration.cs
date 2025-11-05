80e6e73bbd17ff976c85c943d4b307b77042e66a
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-11-06 00:12:16 +0300

    feat(api): set up api layer.

---
+++ OrderProcessing.API/Configuration/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using OrderProcessing.Application.Usecases.PlaceOrder;
+using OrderProcessing.Infrastructure.Configuration;
+
+namespace OrderProcessing.API.Configuration;
+
+public static class ServiceRegistration
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddInfrastructure(config);
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<PlaceOrderCommand>());
+        services.AddValidatorsFromAssemblyContaining<PlaceOrderValidator>();
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+}
---
+++ OrderProcessing.API/Endpoints/PlaceOrderEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+using OrderProcessing.Application.DTOs;
+using OrderProcessing.Application.Usecases.PlaceOrder;
+
+namespace OrderProcessing.API.Endpoints;
+
+public static class PlaceOrderEndpoint
+{
+    public static void MapPlaceOrder(this IEndpointRouteBuilder app)
+    {
+        app.MapPost("/api/orders", async (PlaceOrderDto dto, IValidator<PlaceOrderDto> validator, IMediator mediator) =>
+        {
+            ValidationResult result = await validator.ValidateAsync(dto);
+            if (!result.IsValid)
+            {
+                var errors = result.Errors.Select(e => new { e.PropertyName, e.ErrorMessage });
+                return Results.BadRequest(errors);
+            }
+
+            var command = new PlaceOrderCommand(dto);
+            var orderId = await mediator.Send(command);
+            return Results.Created($"/api/orders/{orderId}", new { orderId });
+        });
+    }
+}
