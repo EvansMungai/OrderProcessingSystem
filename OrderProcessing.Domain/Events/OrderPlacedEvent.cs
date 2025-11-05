8a8e192086680c652b81bbe1de3d5b2fddcd9a69
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-11-05 21:51:13 +0300

    fix(domain): add empty orderplacedevent constructor for deserialization

--- OrderProcessing.Domain/Events/OrderPlacedEvent.cs
+++ OrderProcessing.Domain/Events/OrderPlacedEvent.cs
@@ -2,8 +2,10 @@
 
 public class OrderPlacedEvent
 {
-    public Guid OrderId { get; }
-    public DateTime Timestamp { get; }
+    public Guid OrderId { get; set; }
+    public DateTime Timestamp { get; set; }
+    public decimal TotalAmount { get; set; }
+    public OrderPlacedEvent() { }
     public OrderPlacedEvent(Guid orderId)
     {
         OrderId = orderId;
